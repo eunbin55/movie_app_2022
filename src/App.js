ebb12cfd294716eeb2cc27997a0ddaacaceff266
eunbin55 <eunbin620@naver.com>
2022-09-08 23:54:33 +0900

    04-3 컴포넌트 반복_map()함수

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React from "react";
 
-function Food({fav }) {
-  return <h1>I like {fav}</h1>;
+function Food({name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
 }
+
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png'
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg'
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg'
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://img.sbs.co.kr/newsnet/etv/upload/2014/01/10/30000347969_1280.jpg'
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image: 'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/9jfJ/image/7YSk5b0h1nmdeqIN4crUAWbdwBE'
+  }
+];
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
