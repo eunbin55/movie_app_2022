e9baaa076ae6780c71d5cc4f02adad857ba7443d
eunbin55 <eunbin620@naver.com>
2022-09-12 21:39:47 +0900

    Navigation 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,58 +1,26 @@
-import axios from "axios";
 import React from "react";
-import Movie from "./Movie";
-import './App.css';
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    //async와 await는 짝꿍! 동시에 사용해야 함
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    // console.log(movies);
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              // console.log(movie);
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+        <Routes>
+          <Route path="/" exact={true} element={<Home />}/> 
+          <Route path="/about" element={<About />} />
+          <Route path="movie-detail" element={<Detail />} />
+        </Routes>
+    </HashRouter>
+  )
 }
+
+/**
+ * Route에 exact={true}를 하지 않아도 /about 접속 시 About 컴포넌트만 출력됨...
+ */
 
 export default App;
